3c70d3bae6e4b797e252cef88be5c6b89b0ceead
bofeiw <bofeiw@icloud.com>
2019-05-11 16:42:06 +1000

    add demo

---
+++ js/input.js
@@ -0,0 +1,8 @@
+circleManager = new CircleManager('circles');
+circleManager.add("Hello there!");
+circleManager.add("I'm glad to see you!");
+circleManager.add("How are you?", 1000);
+circleManager.add("This is really a fun demo", 2000);
+circleManager.add("Hope you'll enjoy it!", 5000);
+circleManager.add("Have fun!", 10000);
+circleManager.add("Why are you still there !!!!!!!!!", 20000);
